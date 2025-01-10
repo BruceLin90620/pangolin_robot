6618b6fc13dc8a3a082296ddf4ea62cad0d4851c
Stanley_Chueh <stanleychueh28@gmail.com>
2025-01-10 17:00:49 +0800

    update depthimage_to_laserscan

---
+++ pangolin_visual_slam/depth_to_laserscan_setup/launch/depth_to_laserscan.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='depthimage_to_laserscan',
+            executable='depthimage_to_laserscan_node',
+            name='depthimage_to_laserscan',
+            remappings=[
+                ('/depth', '/camera/camera/depth/image_rect_raw'),
+                ('/depth_camera_info', '/camera/camera/depth/camera_info'),
+            ],
+            parameters=[
+                {'range_min': 0.2},           # Minimum range in meters
+                {'range_max': 5.0},           # Maximum range in meters
+                {'scan_height': 10},          # Number of rows in the depth image to process
+                {'output_frame': 'camera_link'}  # Frame ID for the LaserScan
+            ]
+        )
+    ])
